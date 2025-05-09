98e2c7b86612a8fa801614d8a284996ca5940035
RacheLMacMillan <117262767+RacheLMacMillan@users.noreply.github.com>
2025-05-10 02:01:46 +1100

    Working on state machine system

--- Assets/Core/Systems/StateMachineSystem/BaseState.cs
+++ Assets/Core/Systems/StateMachineSystem/BaseState.cs
@@ -1,6 +1,16 @@
-using UnityEngine;
+using System;
 
-public class BaseState : MonoBehaviour
+public abstract class BaseState<EState> where EState : Enum
 {
-	 
+	public BaseState(EState key)
+	{
+		StateKey = key;
+	}
+	
+	public EState StateKey { get; private set; }
+	
+	public abstract void EnterState();
+	public abstract void ExitState();
+	public abstract void UpdateState();
+	public abstract EState GetNextState();
 }

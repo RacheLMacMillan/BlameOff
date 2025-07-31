94537ab0e1149b93517ed7df9fda3db10e10986d
RacheLMacMillan <117262767+RacheLMacMillan@users.noreply.github.com>
2025-07-31 21:06:19 +1100

    Drawing was fixed

--- Assets/Core/Scripts/Checkers/IsObstacleAboveChecker.cs
+++ Assets/Core/Scripts/Checkers/IsObstacleAboveChecker.cs
@@ -11,9 +11,17 @@ public class IsObstacleAboveChecker : BaseSphereChecker
         return base.CheckArea(_toStandLayer, _positionOfCheck, _radiusOfCheck);
     }
 
-    private void OnDrawGizmos()
+	private void OnDrawGizmos()
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(_positionOfCheck, _radiusOfCheck);
+		Gizmos.color = Color.green;
+
+		Vector3 drawingPosition = new Vector3
+		(
+			transform.position.x + _positionOfCheck.x,
+			transform.position.y + _positionOfCheck.y,
+			transform.position.z + _positionOfCheck.z
+		);
+    
+        Gizmos.DrawWireSphere(drawingPosition, _radiusOfCheck);
     }
 }

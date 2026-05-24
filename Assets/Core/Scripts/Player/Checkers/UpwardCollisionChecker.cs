1681b933f90306e4acab089f369a64e665e85a47
RacheLMacMillan <117262767+RacheLMacMillan@users.noreply.github.com>
2026-05-24 23:25:44 +1100

    Player doesn't stuck in object while crouching or laying

--- Assets/Core/Scripts/Player/Checkers/UpwardCollisionChecker.cs
+++ Assets/Core/Scripts/Player/Checkers/UpwardCollisionChecker.cs
@@ -1,18 +1,51 @@
 using System;
+using Core.Scripts.Player.Movement;
 using UnityEngine;
 
 namespace Core.Scripts.Player.Checkers
 {
     public class UpwardCollisionChecker : MonoBehaviour
     {
-        public Action<bool> OnHitFromAbove;
-        
-        private bool _isHitFromAbove;
-    
+        [Header("Default values")]
         [SerializeField] private LayerMask _detectLayers;
         [SerializeField] private Vector3 _position = new Vector3(0, 1.52f, 0);
         [SerializeField] private float _radius = 0.5f;
+        
+        [Header("Body positions")]
+        [SerializeField] private Vector3 _standingPosition;
+        [SerializeField] private Vector3 _crouchingPosition;
+        [SerializeField] private Vector3 _layingPosition;
     
+        public Action<bool> OnHitFromAbove;
+        
+        private PlayerCharacterHeightController _playerCharacterHeightController;
+        private PlayerCrouch  _playerCrouch;
+        private PlayerLieDowner _playerLieDowner;        
+        
+        private bool _isHitFromAbove;
+        private bool _isDebugging;
+        
+        private void OnEnable()
+        {
+            _playerCharacterHeightController.OnPlayerSandedUp += StandUp;
+            _playerCrouch.OnPlayerCrouched += Crouch;
+            _playerLieDowner.OnPlayerLayDown += LieDown;
+        }
+        
+        private void OnDisable()
+        {
+            _playerCharacterHeightController.OnPlayerSandedUp -= StandUp;
+            _playerCrouch.OnPlayerCrouched -= Crouch;
+            _playerLieDowner.OnPlayerLayDown -= LieDown;
+        }
+
+        private void Awake()
+        {
+            _playerCharacterHeightController = GetComponent<PlayerCharacterHeightController>();
+            _playerCrouch = GetComponent<PlayerCrouch>();
+            _playerLieDowner = GetComponent<PlayerLieDowner>();
+        }
+
         private void Update()
         {
             bool newCheckSphere = Physics.CheckSphere(ScalePosition(_position), _radius, _detectLayers);
@@ -21,7 +54,25 @@ namespace Core.Scripts.Player.Checkers
             {
                 _isHitFromAbove = newCheckSphere;
                 OnHitFromAbove?.Invoke(_isHitFromAbove);
+
+                if (_isDebugging)
+                    Debug.Log("Player was hit from above");
             }
+        }
+
+        private void StandUp()
+        {
+            _position = _standingPosition;
+        }
+
+        private void Crouch()
+        {
+            _position = _crouchingPosition;
+        }
+
+        private void LieDown()
+        {
+            _position = _layingPosition;
         }
 
         private void OnDrawGizmos()
